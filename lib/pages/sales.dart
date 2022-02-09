dc8829a7b9515bde43f3cc9079e3c7cff3bd4730
Ahmet YILDIRIM <ahmetyildirim3@gmail.com>
2022-02-09 13:02:52 +0300

    filtre sayfası tamamlandı

--- lib/pages/sales.dart
+++ lib/pages/sales.dart
@@ -1,11 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-
 import 'package:gazi_app/common/data_repository.dart';
 import 'package:gazi_app/model/customer.dart';
 import 'package:gazi_app/model/sale.dart';
-import 'package:gazi_app/pages/add_customer.dart';
 
 class SalesList extends StatefulWidget {
   late bool selectable;
@@ -31,9 +28,12 @@ String getImagePath(int index) {
 class _SalesListState extends State<SalesList> {
   final _nameController = TextEditingController();
   String _searchText = "";
+  bool _tip1 = true;
+  FilterModel filterModel = new FilterModel();
   @override
   void initState() {
     super.initState();
+    _tip1 = false;
     _nameController.addListener(() {
       if (_nameController.text.isEmpty) {
         setState(() {
@@ -47,6 +47,150 @@ class _SalesListState extends State<SalesList> {
     });
   }
 
+  Future<FilterModel> showFilterDialog(BuildContext context) async {
+    return await showDialog(
+        context: context,
+        builder: (context) {
+          final TextEditingController _textEditingController =
+              TextEditingController();
+          bool isChecked = false;
+          return StatefulBuilder(builder: (context, setState) {
+            return AlertDialog(
+              title: Text('Filtreleme'),
+              content: Container(
+                child: ListView(
+                  children: [
+                    CheckboxListTile(
+                      dense: true,
+                      activeColor: Colors.green,
+                      checkColor: Colors.white,
+                      title: Text("Büyükbaş"),
+                      value: filterModel.buyukbasKurbanSelect,
+                      onChanged: (val) {
+                        setState(() {
+                          filterModel.buyukbasKurbanSelect = val!;
+                        });
+                      },
+                    ),
+                    CheckboxListTile(
+                      dense: true,
+                      activeColor: Colors.green,
+                      checkColor: Colors.white,
+                      title: Text("Küçükbaş"),
+                      value: filterModel.kucukbasKurbanSelect,
+                      onChanged: (val) {
+                        setState(() {
+                          filterModel.kucukbasKurbanSelect = val!;
+                        });
+                      },
+                    ),
+                    Divider(
+                      height: 5.0,
+                    ),
+                    CheckboxListTile(
+                      dense: true,
+                      activeColor: Colors.green,
+                      checkColor: Colors.white,
+                      title: Text("Büyükbaş Ayaktan(Kilo)"),
+                      value: filterModel.buyukAyaktanKilo,
+                      onChanged: (val) {
+                        setState(() {
+                          filterModel.buyukAyaktanKilo = val!;
+                        });
+                      },
+                    ),
+                    CheckboxListTile(
+                      dense: true,
+                      activeColor: Colors.green,
+                      checkColor: Colors.white,
+                      title: Text("Büyükbaş Karkas"),
+                      value: filterModel.buyukKarkas,
+                      onChanged: (val) {
+                        setState(() {
+                          filterModel.buyukKarkas = val!;
+                        });
+                      },
+                    ),
+                    CheckboxListTile(
+                      dense: true,
+                      activeColor: Colors.green,
+                      checkColor: Colors.white,
+                      title: Text("Büyükbaş Ayaktan"),
+                      value: filterModel.buyukAyaktan,
+                      onChanged: (val) {
+                        setState(() {
+                          filterModel.buyukAyaktan = val!;
+                        });
+                      },
+                    ),
+                    CheckboxListTile(
+                      dense: true,
+                      activeColor: Colors.green,
+                      checkColor: Colors.white,
+                      title: Text("Büyükbaş Hisse"),
+                      value: filterModel.buyukHisse,
+                      onChanged: (val) {
+                        setState(() {
+                          filterModel.buyukHisse = val!;
+                        });
+                      },
+                    ),
+                    CheckboxListTile(
+                      dense: true,
+                      activeColor: Colors.green,
+                      checkColor: Colors.white,
+                      title: Text("Küçükbaş Ayaktan(Kilo)"),
+                      value: filterModel.kucukAyaktanKilo,
+                      onChanged: (val) {
+                        setState(() {
+                          filterModel.kucukAyaktanKilo = val!;
+                        });
+                      },
+                    ),
+                    CheckboxListTile(
+                      dense: true,
+                      activeColor: Colors.green,
+                      checkColor: Colors.white,
+                      title: Text("Küçükbaş Ayaktan"),
+                      value: filterModel.kucukAyaktan,
+                      onChanged: (val) {
+                        setState(() {
+                          filterModel.kucukAyaktan = val!;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              actions: [
+                ElevatedButton(
+                    child: Text("Filtreleri Temizle"),
+                    onPressed: () {
+                      setState(() {
+                        filterModel.clear();
+                      });
+                      // your code
+                    }),
+                ElevatedButton(
+                    child: Text("Onayla"),
+                    onPressed: () {
+                      Navigator.of(context).pop(filterModel);
+                    })
+              ],
+            );
+          });
+        });
+  }
+
+  List<QueryDocumentSnapshot<Map<String, dynamic>>> getFilteredResults(
+      List<QueryDocumentSnapshot<Map<String, dynamic>>> saleList,
+      String filter,
+      String filterValue) {
+    return saleList
+        .where((element) => (element.data()[filter].toString() == filterValue))
+        .toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -54,27 +198,37 @@ class _SalesListState extends State<SalesList> {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
+          //
           Padding(
-            padding: EdgeInsets.only(top:40, bottom:0, left:10, right:10),
+            padding: EdgeInsets.only(top: 40, bottom: 0, left: 10, right: 10),
             child: TextField(
-              keyboardType: TextInputType.numberWithOptions(
-                decimal: true,
-                signed: true
-              ),
+              keyboardType:
+                  TextInputType.numberWithOptions(decimal: true, signed: true),
               controller: _nameController,
               decoration:
-                  InputDecoration(labelText: "Kurban numarası ile ara"),
+                  InputDecoration(labelText: "Kurban numarası ile ara "),
             ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               TextButton.icon(
-                  onPressed: () {},
+                  onPressed: () async {
+                    var val = await showFilterDialog(context);
+                    setState(() {
+                      filterModel = val;
+                    });
+                  },
                   icon: Icon(Icons.filter_alt),
                   label: Text("Filtrele")),
               TextButton.icon(
-                  onPressed: () {},
+                  onPressed: () async {
+                    var val = await showFilterDialog(context);
+                    setState(() {
+                      filterModel = val;
+                    });
+                    print(val.toString());
+                  },
                   icon: Icon(Icons.sort),
                   label: Text("Sırala")),
             ],
@@ -87,13 +241,57 @@ class _SalesListState extends State<SalesList> {
                 if (snapshot.hasData) {
                   var saleValues = snapshot.data!.docs.toList();
                   if (_searchText.isNotEmpty) {
-                    saleValues = saleValues
-                        .where((element) => (element
-                                .data()[FieldKeys.saleKurbanNo]
-                                .toString() ==
-                            _searchText))
-                        .toList();
+                    saleValues = getFilteredResults(
+                        saleValues, FieldKeys.saleKurbanNo, _searchText);
+                  } else {
+                    if (filterModel.buyukbasKurbanSelect &&
+                        !filterModel.kucukbasKurbanSelect) {
+                      saleValues = getFilteredResults(
+                          saleValues, FieldKeys.saleKurbanTip, "1");
+                    } else if (!filterModel.buyukbasKurbanSelect &&
+                        filterModel.kucukbasKurbanSelect) {
+                      saleValues = getFilteredResults(
+                          saleValues, FieldKeys.saleKurbanTip, "2");
+                    }
+                    if (filterModel.buyukAyaktan ||
+                        filterModel.buyukAyaktanKilo ||
+                        filterModel.buyukHisse ||
+                        filterModel.buyukKarkas ||
+                        filterModel.kucukAyaktan ||
+                        filterModel.kucukAyaktanKilo) {
+                      List<String> filteredList = [];
+                      if (filterModel.buyukAyaktanKilo) {
+                        filteredList.add("1");
+                      }
+                      if (filterModel.buyukKarkas) {
+                        filteredList.add("2");
+                      }
+                      if (filterModel.buyukAyaktan) {
+                        filteredList.add("3");
+                      }
+                      if (filterModel.buyukHisse) {
+                        filteredList.add("4");
+                      }
+                      if (filterModel.kucukAyaktanKilo) {
+                        filteredList.add("5");
+                      }
+                      if (filterModel.kucukAyaktan) {
+                        filteredList.add("6");
+                      }
+                      var firstList = getFilteredResults(saleValues,
+                          FieldKeys.saleKurbanSubTip, filteredList[0]);
+                      var finalList = firstList;
+                      for (int i = 1; i < filteredList.length; i++) {
+                        firstList = getFilteredResults(saleValues,
+                            FieldKeys.saleKurbanSubTip, filteredList[i]);
+                        finalList += firstList;
+                      }
+                      finalList.sort((a, b) => a[FieldKeys.saleKurbanNo]
+                          .compareTo(b[FieldKeys.saleKurbanNo]));
+                      saleValues = finalList;
+                    }
                   }
+
                   //.snapshot.value;
                   return ListView.builder(
                     itemCount: saleValues.length,
@@ -132,5 +330,28 @@ class _SalesListState extends State<SalesList> {
         ],
       ),
     );
+  }
+}
+
+class FilterModel {
+  bool buyukbasKurbanSelect = false;
+  bool kucukbasKurbanSelect = false;
+  bool buyukAyaktanKilo = false;
+  bool buyukKarkas = false;
+  bool buyukAyaktan = false;
+  bool buyukHisse = false;
+  bool kucukAyaktanKilo = false;
+  bool kucukAyaktan = false;
+  FilterModel();
+
+  void clear() {
+    this.buyukbasKurbanSelect = false;
+    this.kucukbasKurbanSelect = false;
+    this.buyukAyaktanKilo = false;
+    this.buyukKarkas = false;
+    this.buyukAyaktan = false;
+    this.buyukHisse = false;
+    this.kucukAyaktanKilo = false;
+    this.kucukAyaktan = false;
   }
 }
